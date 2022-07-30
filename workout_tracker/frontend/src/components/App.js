3928d8b8160b0796597a41e62529fbe29500b11e
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-07-30 12:31:20 -0700

    successfully renders new components

--- workout_tracker/frontend/src/components/App.js
+++ workout_tracker/frontend/src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react"
 import { render } from "react-dom"
+import HomePage from "./HomePage"
+import Workout from "./WorkoutPage"
+import CreateWorkout from "./CreateWorkout"
+
 
 export default class App extends Component {
     constructor(props) {
@@ -7,7 +11,13 @@ export default class App extends Component {
     }
 
     render() {
-        return (<h1>Testing React Code</h1>)
+        return (
+        <div>
+            <HomePage />
+            <CreateWorkout />
+            <Workout />
+        </div>
+        )
     }
 }
 
---
+++ workout_tracker/frontend/src/components/WorkoutPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+
+
+export default class Workout extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <p>This is the Workout page</p>
+    }
+}

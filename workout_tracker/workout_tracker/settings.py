6b5373f58688fbd78266cbf23831e26dc52c4445
RawRaw5 <94robertwhite@gmail.com>
2022-07-30 10:15:41 -0700

    stashing files

--- workout_tracker/workout_tracker/settings.py
+++ workout_tracker/workout_tracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d!_57&3tn6@-aoy=qj*pc=&@z(#k-8li&yn(xp%)1u0hso%!kk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

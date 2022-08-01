e0f658dfad06bbd52fcdf0d36913afcf45ec8424
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-08-01 11:56:38 -0700

    added reactstrap to frontend and made base createworkout page

--- workout_tracker/frontend/src/components/CreateWorkout.js
+++ workout_tracker/frontend/src/components/CreateWorkout.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import { Form, Row, Input, Label, FormGroup, Col, Button } from 'reactstrap'
 
 
 export default class CreateWorkout extends Component {
@@ -7,6 +8,52 @@ export default class CreateWorkout extends Component {
     }
 
     render() {
-        return <p>This is the Create Workout page</p>
+        return (
+        <Form>
+            <Row>
+            <Col md={6}>
+                <FormGroup>
+                <Label for="exercise">
+                    Exercise
+                </Label>
+                <Input
+                    id="exercise"
+                    name="exercise"
+                    placeholder="enter exercise here"
+                    type="exercise"
+                />
+                </FormGroup>
+            </Col>
+            <Col md={6}>
+                <FormGroup>
+                <Label for="reps">
+                    Number of Reps
+                </Label>
+                <Input
+                    id="reps"
+                    name="reps"
+                    placeholder="enter amount of reps"
+                    type="number"
+                />
+                </FormGroup>
+            </Col>
+            </Row>
+            <FormGroup>
+            <Label for="sets">
+                Number of Sets
+            </Label>
+            <Input
+                id="sets"
+                name="sets"
+                placeholder="enter amount of sets"
+                type="number"
+            />
+            </FormGroup>
+            <Button>
+            Record Exercise
+            </Button>
+        </Form>
+      )
+      
     }
 }

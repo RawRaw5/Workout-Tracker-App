5568767b33598754ce1c8615a0524e0c43d00ab2
RawRaw5 <94robertwhite@gmail.com>
2022-08-03 16:38:27 -0700

    updating the home page style

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -11,7 +11,21 @@ export default class HomePage extends Component {
 
     render() {
         return (
-            <h1>homepage</h1>
+            <main>
+                <h1>Workout Tracking App</h1>
+                    <div className="home">  
+                        <p>Welcome to Workout Tracking App where you can create your daily workouts</p>
+                    </div>
+                    <div className="about">
+                        <h2>Description</h2>
+                            <p>We both had the role of being Full-stack Developers for this project. For the frontend we used React and for backend we used Python with Django. We decided to implement Django because it’s a framework neither of us were familiar with and didn’t learn in our bootcamp course, so we wanted to challenge ourselves and learn something new. Facing many challenges and issues, we had the opportunity to learn a lot through trail and error. A few of those issues that we found solutions to were coding with Python/Django, Git commands, server deployment and connecting frontend - backend - sever - database - showing on frontend. </p>
+                    </div>
+                    <div className="technologies">
+                        <h2>Technologies</h2>
+                            <p>Frontend - React, Javascript, Material UI, React-router-dom Babel, Webpack</p>
+                            <p>Backend - Python, Django, db.sqlite3 </p>
+                    </div>
+            </main>    
         )
     }
 }

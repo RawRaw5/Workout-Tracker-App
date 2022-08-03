d0b0ef9dee8234de8f3f259a2dfc4c0bb7f8c2a7
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-08-03 14:22:07 -0700

    creating post request for frontend

--- frontend/src/components/CreateWorkout.js
+++ frontend/src/components/CreateWorkout.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react"
-import { Form, Row, Input, Label, FormGroup, Col, Button } from 'reactstrap'
-
+import { Form, Row, Input, Label, FormGroup, Col } from 'reactstrap'
+import { Button } from "@material-ui/core"
+import  Grid  from "@material-ui/core/Grid"
+import { Typography } from "@material-ui/core/"
+import TextField from "@material-ui/core/"
+import FormHelperText from "@material-ui/core/"
+import FormControl from "@material-ui/core/"
+import { Link } from "react-router-dom"
+import { Radio } from "@material-ui/core/"
+import { RadioGroup } from "@material-ui/core/"
+import { FormControlLabel } from "@material-ui/core/"
 
 export default class CreateWorkout extends Component {
     constructor(props) {
@@ -8,45 +17,14 @@ export default class CreateWorkout extends Component {
     }
 
     render() {
-        return (
-        <Form className="exercise-form">
-            <Label>Exercise Form</Label>
-            <Row/>
-            <Label for="exercise" className="exercise">Exercise</Label>
-            <Input
-                id="exercise"
-                name="exercise"
-                placeholder="enter exercise here"
-                type="name"
-                required
-            />
-            <Row/>
-            <Label for="reps" className="reps">Number of Reps</Label>
-            <Input
-                id="reps"
-                name="reps"
-                placeholder="enter number of reps"
-                type="number"
-                required
-            />
-            <Row/>
-            <Label for="sets" className="sets">Number of Sets</Label>
-            <Input
-                id="sets"
-                name="sets"
-                placeholder="enter number of sets"
-                type="number"
-                required
-            />
-            <Row/>
-            <Button>
-                Record Exercise
-            </Button>
-            <Button>
-                Finish Workout
-            </Button>
-        </Form>
-      )
+        return  <Grid container spacing={1}>
+            <Grid item xs={12} align="center">
+                <Typography component='h4' variant='h4'>
+                    Create A Workout
+                </Typography>
+            </Grid>
+        </Grid>
+      
       
     }
 }

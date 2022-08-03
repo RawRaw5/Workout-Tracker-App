b210c8c154ec9f657f77d5ff0fa0e2d801b4b9c8
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-08-03 14:39:03 -0700

    created workoutform

--- frontend/src/components/CreateWorkout.js
+++ frontend/src/components/CreateWorkout.js
@@ -3,9 +3,9 @@ import { Form, Row, Input, Label, FormGroup, Col } from 'reactstrap'
 import { Button } from "@material-ui/core"
 import  Grid  from "@material-ui/core/Grid"
 import { Typography } from "@material-ui/core/"
-import TextField from "@material-ui/core/"
-import FormHelperText from "@material-ui/core/"
-import FormControl from "@material-ui/core/"
+import TextField from "@material-ui/core/TextField"
+import FormHelperText from "@material-ui/core/FormHelperText"
+import FormControl from "@material-ui/core/FormControl"
 import { Link } from "react-router-dom"
 import { Radio } from "@material-ui/core/"
 import { RadioGroup } from "@material-ui/core/"
@@ -22,6 +22,30 @@ export default class CreateWorkout extends Component {
                 <Typography component='h4' variant='h4'>
                     Create A Workout
                 </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <FormControl component="fieldset">
+                    <div align="center">Exercise</div>
+                    <br></br>
+                    <TextField required={true} type="text" inputProps={{style: { textAlign: "center"}
+                    }}/>
+                    <br></br>
+                    <div align="center">Number of Reps</div>
+                    <br></br>
+                    <TextField required={true} type="number" inputProps={{min:1, style: { textAlign: "center"}
+                    }}/>
+                    <br></br>
+                    <div align="center">Number of Sets</div>
+                    <br></br>
+                    <TextField required={true} type="number" inputProps={{min:1, style: { textAlign: "center"}
+                    }}/>
+                    <br></br>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Button color="primary" variant="contained">
+                    Create Exercise
+                </Button>
             </Grid>
         </Grid>
       
